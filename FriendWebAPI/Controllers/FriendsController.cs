827a3dfe8025e400cc4110957bf9847e7ab5c5df
Markeesi <62094844+Markeesi@users.noreply.github.com>
2023-10-08 16:39:59 +0300

    Added NLogger to the project.

--- FriendWebAPI/Controllers/FriendsController.cs
+++ FriendWebAPI/Controllers/FriendsController.cs
@@ -1,4 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using NLog; // Import NLog namespace
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Text.Json;
 
 namespace FriendWebAPI.Controllers
@@ -7,7 +12,9 @@ namespace FriendWebAPI.Controllers
     [Route("[controller]")]
     public class FriendsController : ControllerBase
     {
-        private readonly IFriendsRepository _friendsRepository;  
+        private readonly IFriendsRepository _friendsRepository;
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger(); // Initialize the NLog logger
+
         public FriendsController(IFriendsRepository friendsRepository)
         {
             _friendsRepository = friendsRepository;
@@ -16,59 +23,91 @@ namespace FriendWebAPI.Controllers
         [HttpGet("json")]
         public IActionResult GetJsonFileContent()
         {
-            // Get the path to the JSON file
-            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "friends.json");
-
-            // Read the content from the file (if it exists)
-            if (System.IO.File.Exists(filePath))
+            try
             {
-                string fileContent = System.IO.File.ReadAllText(filePath);
-                return Content(fileContent, "application/json");
+                // Get the path to the JSON file
+                string filePath = Path.Combine(Directory.GetCurrentDirectory(), "friends.json");
+
+                // Read the content from the file (if it exists)
+                if (System.IO.File.Exists(filePath))
+                {
+                    string fileContent = System.IO.File.ReadAllText(filePath);
+                    _logger.Info("GET JSON file content - Status Code: 200 OK");
+                    return Content(fileContent, "application/json");
+                }
+
+                _logger.Warn("GET JSON file content - Status Code: 404 Not Found");
+                return NotFound(); // File not found
             }
-
-            return NotFound(); // File not found
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "An error occurred while retrieving JSON content.");
+                return StatusCode(500, "Internal Server Error");
+            }
         }
-
 
         [HttpPost]
         public IActionResult CreateFriends([FromBody] CreateFriendRequest request)
         {
-            if (request == null)
+            try
             {
-                return BadRequest("Invalid request body.");
+                if (request == null)
+                {
+                    _logger.Warn("POST CreateFriends - Status Code: 400 Bad Request - Invalid request body.");
+                    return BadRequest("Invalid request body.");
+                }
+
+                // Validate the incoming request for unexpected properties
+                var validationContext = new ValidationContext(request, serviceProvider: null, items: null);
+                var validationResults = new List<ValidationResult>();
+                var isValid = Validator.TryValidateObject(request, validationContext, validationResults, validateAllProperties: true);
+
+                if (!isValid)
+                {
+                    foreach (var validationResult in validationResults)
+                    {
+                        _logger.Warn($"POST CreateFriends - Status Code: 400 Bad Request - {validationResult.ErrorMessage}");
+                    }
+
+                    return BadRequest(validationResults.Select(vr => vr.ErrorMessage));
+                }
+
+                // Create a new FriendCombination instance
+                var newCombination = new FriendCombination
+                {
+                    Friend1 = request.friend1,
+                    Friend2 = request.friend2
+                };
+
+                // Get the path to the JSON file
+                string filePath = Path.Combine(Directory.GetCurrentDirectory(), "friends.json");
+
+                // Deserialize existing content from the file (if it exists)
+                List<FriendCombination> existingCombinations = new List<FriendCombination>();
+                if (System.IO.File.Exists(filePath))
+                {
+                    string existingContent = System.IO.File.ReadAllText(filePath);
+                    existingCombinations = JsonSerializer.Deserialize<List<FriendCombination>>(existingContent);
+                }
+
+                // Add the new friend combination
+                existingCombinations.Add(newCombination);
+
+                // Serialize the updated content back to JSON
+                string updatedContent = JsonSerializer.Serialize(existingCombinations, new JsonSerializerOptions { WriteIndented = true });
+
+                // Write the updated content back to the file
+                System.IO.File.WriteAllText(filePath, updatedContent);
+
+                _logger.Info("POST CreateFriends - Status Code: 200 OK - Combined friends appended to the JSON file.");
+                return Ok("Combined friends appended to the JSON file.");
             }
-
-            // Create a new FriendCombination instance
-            var newCombination = new FriendCombination
+            catch (Exception ex)
             {
-                Friend1 = request.friend1,
-                Friend2 = request.friend2
-            };
-
-            // Get the path to the JSON file
-            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "friends.json");
-
-            // Deserialize existing content from the file (if it exists)
-            List<FriendCombination> existingCombinations = new List<FriendCombination>();
-            if (System.IO.File.Exists(filePath))
-            {
-                string existingContent = System.IO.File.ReadAllText(filePath);
-                existingCombinations = JsonSerializer.Deserialize<List<FriendCombination>>(existingContent);
+                _logger.Error(ex, "An error occurred while creating friends.");
+                return StatusCode(500, "Internal Server Error");
             }
-
-            // Add the new friend combination
-            existingCombinations.Add(newCombination);
-
-            // Serialize the updated content back to JSON
-            string updatedContent = JsonSerializer.Serialize(existingCombinations, new JsonSerializerOptions { WriteIndented = true });
-
-            // Write the updated content back to the file
-            System.IO.File.WriteAllText(filePath, updatedContent);
-
-            // Return a success response
-            return Ok("Combined friends appended to the JSON file.");
         }
-
 
     }
 }
